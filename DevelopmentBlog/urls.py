0e3871a1ae98481a9aa9452e3588e941e262ba15
Matthew Leonard <mleonard0197@gmail.com>
2023-01-10 15:20:06 -0700

    Add style.css and add page extensions

--- DevelopmentBlog/urls.py
+++ DevelopmentBlog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from pages.views import home_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogs/', include('blogs.urls'))
+    path('blogs/', include('blogs.urls')),
+    path('', home_view, name='home')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -7,3 +7,4 @@ from .models import Post
 def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
+
